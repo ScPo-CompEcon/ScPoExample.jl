9416b0b607858495b9e2e87542142df89f74029c
Florian Oswald <florian.oswald@gmail.com>
2017-02-09 18:20:57 +0100

    updates

--- src/ScPoExample.jl
+++ src/ScPoExample.jl
@@ -3,7 +3,4 @@ export hello, domath
 
 hello(who::String) = "Hello, $who"
 domath(x::Number) = (x + 5)
-get_rand() = rand(2,3,3)
-
-include("dbg.jl")
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -4,4 +4,3 @@ using Base.Test
 @test hello("Julia") == "Hello, Julia"
 @test_approx_eq domath(2.0) 7.0
 
-@test size(ScPoExample.get_rand()) == (2,3,3)
